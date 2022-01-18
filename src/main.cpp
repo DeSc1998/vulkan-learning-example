d5ef5373b1341e114f5e20aa7d443c1cc1d5a303
Dennis Schulze <dennis@axelschulze.net>
2022-01-19 00:15:16 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+
+#include "pch.hpp"
+
+#define Unused(x) (void)x
+
+int main(int argc, char *argv[]) {
+
+  Unused(argc);
+  Unused(argv);
+
+  std::cout << "Hello World\n";
+
+  return 0;
+}
---
+++ src/pch.hpp
@@ -0,0 +1,8 @@
+
+
+#ifndef PCH_H_
+#define PCH_H_
+
+#include <iostream>
+
+#endif // PCH_H_
