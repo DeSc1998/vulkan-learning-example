601a319ea6f46738ded8f15b5a863e8fc2049932
Dennis Schulze <dennis@axelschulze.net>
2023-04-07 18:15:31 +0200

    formated shaders

--- shader/fragment.hlsl
+++ shader/fragment.hlsl
@@ -3,10 +3,8 @@
 #pragma fragment main
 
 struct v2p {
-    float4 pos : SV_POSITION;
-    float4 color : COLOR;
+  float4 pos : SV_POSITION;
+  float4 color : COLOR;
 };
 
-float4 main(v2p input) {
-    return input.color;
-}
+float4 main(v2p input) { return input.color; }
--- shader/vertex.hlsl
+++ shader/vertex.hlsl
@@ -2,48 +2,35 @@
 #pragma vertex main
 
 struct info {
-    float4 pos : POSITION;
-    float4 color : COLOR;
-    float time : PSIZE;
+  float4 pos : POSITION;
+  float4 color : COLOR;
+  float time : PSIZE;
 };
 
 struct v2p {
-    float4 pos : SV_POSITION;
-    float4 color : COLOR;
+  float4 pos : SV_POSITION;
+  float4 color : COLOR;
 };
 
-float4 rotate_z( float4 pos, float4 axes, float angle ) {
-    float4x4 rotate = {
-        cos(angle),  sin(angle), 0.0, 0.0,
-        -sin(angle), cos(angle), 0.0, 0.0,
-        0.0,         0.0,        1.0, 0.0,
-        0.0,         0.0,        0.0, 1.0
-    };
+float4 rotate_z(float4 pos, float4 axes, float angle) {
+  float4x4 rotate = {cos(angle), sin(angle), 0.0, 0.0, -sin(angle), cos(angle),
+                     0.0,        0.0,        0.0, 0.0, 1.0,         0.0,
+                     0.0,        0.0,        0.0, 1.0};
 
-    return rotate * (pos - axes) + axes;
+  return rotate * (pos - axes) + axes;
 }
 
-float4 rotate_y( float4 pos, float4 axes, float angle ) {
-    float4x4 rotate = {
-        cos(angle),  0, sin(angle), 0,
-        0,           1, 0,          0,
-        -sin(angle), 0, cos(angle), 0,
-        0,           0, 0,          1
-    };
+float4 rotate_y(float4 pos, float4 axes, float angle) {
+  float4x4 rotate = {cos(angle),  0, sin(angle), 0, 0, 1, 0, 0,
+                     -sin(angle), 0, cos(angle), 0, 0, 0, 0, 1};
 
-    return rotate * (pos - axes) + axes;
+  return rotate * (pos - axes) + axes;
 }
 
-
-
-v2p main( info input ) {
-    v2p output;
-    const float pi = 3.14159265358979323846;
-    output.pos =
-        rotate_y(
-            input.pos,
-            float4(0.0, 0.0, 0.5, 0.0),
-            pi * input.time);
-    output.color = input.color;
-    return output;
+v2p main(info input) {
+  v2p output;
+  const float pi = 3.14159265358979323846;
+  output.pos = rotate_y(input.pos, float4(0.0, 0.0, 0.5, 0.0), pi * input.time);
+  output.color = input.color;
+  return output;
 }
